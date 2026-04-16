b3e1746f42b8ca3fa7526e9b0604a4a071eefbf5
unknown <benabidsana@gmai.com>
2026-04-16 09:36:33 +0100

    new task

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,12 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿ using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tp3EFcoreRelations.data;
+using Tp3EFcoreRelations.Models;
 
 namespace Tp3EFcoreRelations.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CustomerController(ApplicationDbContext context) {  
+            _context = context; 
+        }
         public IActionResult Index()
         {
+            var customers = _context.Customers.Include(c => c.membeshipType).ToList();
+            return View(customers);
+        }
+        //Get :Customers/Create
+        public IActionResult Create()
+        {
+            var members = _context.MembershipTypes.ToList();
+            ViewBag.member = new SelectList(members, "Id", "Name");
             return View();
         }
+
+        //POST : Customers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CustomerId,Name,MembershiptypeID")] Customer customer)
+        {
+            //verfier si MembershptypeID est null
+            /*if (customer.membeshipType)
+            {
+                ModelState.AddModelError("MembershipType", "Le type d'adhesion");
+            }*/
+
+            if (ModelState.IsValid)
+            {
+                customer.CustomerId = Guid.NewGuid(); //Generation d'un nouvel ID
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            var members = _context.MembershipTypes.ToList();
+            ViewBag.member = new SelectList(members, "Id", "Name");
+            return View(customer);
+
+        }
+
+        //Get :Customers/Edit/5
+        public IActionResult Edit(Guid?id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            var members = _context.MembershipTypes.ToList();
+            ViewBag.member = new SelectList(members, "Id", "Name",customer);
+            return View(customer);
+        }
+
+        
+
+        /*public IActionResult Edit()
+        {
+            _context.Update(customer);
+            _context.SaveChanges();
+        }*/
+
     }
 }
---
+++ Controllers/MembershipTypesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Tp3EFcoreRelations.Models;
+using Tp3EFcoreRelations.data;
+
+namespace Tp3EFcoreRelations.Controllers
+{
+    public class MembershipTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembershipTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MembershipTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MembershipTypes.ToListAsync());
+        }
+
+        // GET: MembershipTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        // GET: MembershipTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MembershipTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,SingUpFree,DurationInMonth,DiscountRate")] MembershipType membershipType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(membershipType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+
+        // GET: MembershipTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes.FindAsync(id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+            return View(membershipType);
+        }
+
+        // POST: MembershipTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,SingUpFree,DurationInMonth,DiscountRate")] MembershipType membershipType)
+        {
+            if (id != membershipType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(membershipType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembershipTypeExists(membershipType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(membershipType);
+        }
+
+        // GET: MembershipTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var membershipType = await _context.MembershipTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (membershipType == null)
+            {
+                return NotFound();
+            }
+
+            return View(membershipType);
+        }
+
+        // POST: MembershipTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var membershipType = await _context.MembershipTypes.FindAsync(id);
+            if (membershipType != null)
+            {
+                _context.MembershipTypes.Remove(membershipType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembershipTypeExists(int id)
+        {
+            return _context.MembershipTypes.Any(e => e.Id == id);
+        }
+    }
+}
